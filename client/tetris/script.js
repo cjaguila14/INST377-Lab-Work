ec1baa7a596673e88cee6eec6ae0c1050a2902ee
cjaguila14 <43657623+cjaguila14@users.noreply.github.com>
2022-02-15 16:22:49 -0500

    still working on tetris

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,13 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-
-
-
-
-
-
-
-
-
-
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+});
