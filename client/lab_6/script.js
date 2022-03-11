74eebd0cee67af57f0ed6c00836488177c4dbbe3
cjaguila14 <43657623+cjaguila14@users.noreply.github.com>
2022-03-11 12:25:20 -0500

    followed along with tutorial. DataHandler is setup.

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,23 @@
+function dataHandler(arr) {
+  console.log('Fired Data Handler');
+  console.table(arr); // this is called "dot notation"
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+  const form = document.querySelector('#main-form');
+  const submit = document.querySelector('.submit-button');
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  if (arrayFromJson.data.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      submit.style.display = 'block';
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      dataHandler(arrayFromJson.data);
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
