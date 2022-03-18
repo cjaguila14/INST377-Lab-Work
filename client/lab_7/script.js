f56a78ff814116d4c170a6621d68f25cd56c9daf
cjaguila14 <43657623+cjaguila14@users.noreply.github.com>
2022-03-18 16:19:12 -0400

    Watched walkthrough and finished lab 7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -6,7 +6,7 @@ function getRandomIntInclusive(min, max) {
 }
 
 function restoArrayGenerator(arr) {
-  console.log('Fired Restaurant Array Generator');
+  // console.log('Fired Restaurant Array Generator');
   console.table(arr); // this is called "dot notation"
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
@@ -17,8 +17,8 @@ function restoArrayGenerator(arr) {
 }
 
 function generateHtmlList(collection) {
-  console.log('Fired HTML Generator');
-  console.log(collection);
+  // console.log('Fired HTML Generator');
+  // console.log(collection);
   const targetList = document.querySelector('.resto-list');
   targetList.innerHTML = '';
   collection.forEach((item) => {
@@ -30,21 +30,55 @@ function generateHtmlList(collection) {
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
   const form = document.querySelector('#main-form');
   const submit = document.querySelector('.submit-button');
+  const resto = document.querySelector('#resto_name');
+  const zipcode = document.querySelector('#zipcode');
   submit.style.display = 'none';
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  console.log(arrayFromJson);
+  // console.log(arrayFromJson);
+  // The if statement is to prevent race condition on dataload
   if (arrayFromJson.data.length > 0) {
     submit.style.display = 'block';
+    let currentArray = [];
+    resto.addEventListener('input', async (event) => {
+      if (currentArray === undefined) { return; }
+      console.log(event.target.value);
+      if (currentArray.length < 1) {
+        // console.log('empty);
+        return;
+      }
+      const matchResto = currentArray.filter((item) => {
+        const lowerName = item.name.toLowerCase();
+        const lowerValue = event.target.value.toLowerCase();
+        return lowerName.includes(lowerValue);
+      });
+      generateHtmlList(matchResto);
+      // console.log(matchResto);
+    });
+    zipcode.addEventListener('input', async (event) => {
+      if (currentArray === undefined) { return; }
+      console.log(event.target.value);
+      if (currentArray.length < 1) {
+        // console.log('empty);
+        return;
+      }
+      const matchZip = currentArray.filter((item) => {
+        const itemZip = item.zip;
+        const itemValue = event.target.value;
+        return itemZip.includes(itemValue);
+      });
+      generateHtmlList(matchZip);
+    });
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
-      console.log('form submission'); // this is substituting for a "breakpoint"
+      // console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      const restoArray = restoArrayGenerator(arrayFromJson.data);
-      generateHtmlList(restoArray);
+      currentArray = restoArrayGenerator(arrayFromJson.data);
+      generateHtmlList(currentArray);
     });
   }
 }
