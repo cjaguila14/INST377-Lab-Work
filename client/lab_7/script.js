11259dd05e716160be577bcea9e9508ed7595592
cjaguila14 <43657623+cjaguila14@users.noreply.github.com>
2022-03-18 13:30:33 -0400

    copied everythign from lab 6 to lab 7

---
+++ client/lab_7/script.js
@@ -0,0 +1,53 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+  // The maximum is inclusive and the minimum is inclusive
+}
+
+function restoArrayGenerator(arr) {
+  console.log('Fired Restaurant Array Generator');
+  console.table(arr); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const randResto = getRandomIntInclusive(0, arr.length - 1);
+    return arr[randResto];
+  });
+  return listItems;
+}
+
+function generateHtmlList(collection) {
+  console.log('Fired HTML Generator');
+  console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+async function mainEvent() { // the async keyword means we can make API requests
+  const form = document.querySelector('#main-form');
+  const submit = document.querySelector('.submit-button');
+  submit.style.display = 'none';
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+  if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      const restoArray = restoArrayGenerator(arrayFromJson.data);
+      generateHtmlList(restoArray);
+    });
+  }
+}
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
